04f6905c0d796bbeb3ea10289a0b76468af2c346
David Hsu <dvdhsu@gmail.com>
2015-12-11 19:51:48 +0000

    Change port for deploying.

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :od_listings, OdListings.Endpoint,
-  http: [port: 4000],
+  http: [port: 4005],
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
